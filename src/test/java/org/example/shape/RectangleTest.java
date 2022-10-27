de7621535e4f3ddf02bf7bfc3a8ea3e76d04a4ba
Oleksandr Popovych <oleksandrpopovych89@gmail.com>
2022-10-27 13:05:25 +0300

    Added first Unit test.

--- src/test/java/org/example/shape/RectangleTest.java
+++ src/test/java/org/example/shape/RectangleTest.java
@@ -11,5 +11,6 @@ class RectangleTest {
         assertEquals(200, new Rectangle(20, 10).getSquare());
         assertEquals(200, new Rectangle(10, 20).getSquare());
         assertEquals(100, new Rectangle(5, 20).getSquare());
+        assertEquals(100, new Rectangle(-100, 10).getSquare());
     }
 }

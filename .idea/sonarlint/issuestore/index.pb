8fc1690c0d9cfd07651a2fac7c98436ba42190a9
Oleksandr Popovych <oleksandrpopovych89@gmail.com>
2022-10-28 10:01:36 +0300

    Added all Unit tests.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+gradlew,5\b\5bbfa66edb4db3c7c33c5181f43510990d3307f9
+Z
+*src/main/java/org/example/shape/Shape.java,c\f\cf47c7b48622df5577ae18ce9d7e5a1ecc721a05
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,14 @@
 package org.example;
 
-import org.example.shape.Rectangle;
 import org.example.shape.Trapezoid;
-import org.example.shape.Triangle;
 
 public class Main {
     public static void main(String[] args) {
 
-        Rectangle rectangle = new Rectangle(25, 10);
-        Triangle triangle = new Triangle(10, 20);
 
-        Trapezoid trapezoid1 = new Trapezoid(10, 30, 10);
-        Trapezoid trapezoid2 = new Trapezoid(30, 10, 10);
+        Trapezoid trapezoid = new Trapezoid(4.15, 2.2, 2.8);
 
-        System.out.println(rectangle.getSquare());
-        System.out.println(triangle.getSquare());
-        System.out.println(trapezoid1.getSquare());
-        System.out.println(trapezoid2.getSquare());
+
+        System.out.println(trapezoid.getSquare()/2.2);
     }
 }
